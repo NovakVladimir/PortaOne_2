08636380c514e321f2517d4f20ba7b4f11124f36
NovakVladimir <vladimir.novak.89@i.ua>
2022-06-19 23:56:22 +0300

    optimize js, add waiting result

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ const grow = (ar) => {
               res = subRes.slice();
           } 
           subRes = []
-          
       }
   }
+  ar="";
   return res.join(", ");
 }
 
@@ -43,39 +43,42 @@ const fall = (ar) => {
           subRes = []
       }
   }
+  ar="";
   return res.join(", ");
 }
 
 const makeResult = (inputData) => {
-  let changeInputData= inputData.split("\n").map(el=>+el);
-  let middle = changeInputData.reduce((sum,el) => sum+=el,0) / changeInputData.length;
-  let growSubsequence = grow(changeInputData);
-  let fallSubsequence = fall(changeInputData);
-  let sortArray = changeInputData.sort((a,b)=>a-b);
-  let maximal =  sortArray[sortArray.length-1];
-  let minimal = sortArray[0];
-  let mediana =  sortArray.length%2===0? ( sortArray[ sortArray.length/2-1] +  sortArray[ sortArray.length/2])/2 
-  :  sortArray[(sortArray.length-1)/2]
+    let changeInputData= inputData.split("\n").map(el=>+el);
+    let middle = changeInputData.reduce((sum,el) => sum+=el,0) / changeInputData.length;
+    let growSubsequence = grow(changeInputData);
+    let fallSubsequence = fall(changeInputData);
+    let sortArray = changeInputData.sort((a,b)=>a-b);
+    changeInputData = "";
+    let maximal =  sortArray[sortArray.length-1];
+    let minimal = sortArray[0];
+    let mediana =  sortArray.length%2===0? ( sortArray[ sortArray.length/2-1] +  sortArray[ sortArray.length/2])/2 
+    :  sortArray[(sortArray.length-1)/2];
+    sortArray = "";
+   
+    
+    return [maximal, minimal, mediana, middle, growSubsequence, fallSubsequence];
+  }
   
-  return [maximal, minimal, mediana, middle, growSubsequence, fallSubsequence];
-}
-
-
-
-// Block for reading file and start all functions
-
-const handleFileReader = (e) => {
-    let result = makeResult(fileReader.result);
-    const name = ["1. Максимальне число в файлі: ", "2. Мінімальне число в файлі: ", "3. Медіана: ", "4. Cереднє арифметичне значення: ", "5. Найбільша послідовність чисел (які ідуть один за одним), яка збільшується: ", "6. Найбільша послідовність чисел (які ідуть один за одним), яка зменшується: "];
-    result = result.map((el,ind) => `${name[ind]} ${el};`).join("\n");
-    console.log(result);
-    output.innerText = result;
-}
-
-const handleFileChosen = (file) => {
-    fileReader = new FileReader();
-    fileReader.onloadend = handleFileReader;
-    return fileReader.readAsText(file);
-}
-
-input.addEventListener("change", (e) => handleFileChosen(e.target.files[0]));
+  
+  
+  // Block for reading file and start all functions
+  
+  const handleFileReader = (e) => {
+      const name = ["1. Максимальне число в файлі: ", "2. Мінімальне число в файлі: ", "3. Медіана: ", "4. Cереднє арифметичне значення: ", "5. Найбільша послідовність чисел (які ідуть один за одним), яка збільшується: ", "6. Найбільша послідовність чисел (які ідуть один за одним), яка зменшується: "];
+      result = makeResult(fileReader.result).map((el,ind) => `${name[ind]} ${el};`).join("\n");
+      output.innerText = result;
+  }
+  
+  const handleFileChosen = (file) => {
+      fileReader = new FileReader();
+      fileReader.onloadend = handleFileReader;
+      return fileReader.readAsText(file);
+  }
+  input.addEventListener("click", () => output.innerText = "Please, wait a few seconds!");
+  input.addEventListener("change", (e) => handleFileChosen(e.target.files[0]));
+  
